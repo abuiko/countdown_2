8e237b173074402eadd0b7cfb5177e56fc945036
Alena Buiko <abuiko.ma@gmail.com>
2021-03-11 22:12:55 -0500

    update content p with js

--- app.js
+++ app.js
@@ -22,3 +22,19 @@ const weekdays = [
     'Friday',
     'Saturday'
 ];
+
+const countdown = document.querySelector('countdown');
+const items = document.querySelectorAll('.content h4');
+const content = document.querySelector('.content p');
+
+
+let futureDate = new Date('2021-06-01T05:30:00');
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+const date = futureDate.getDate();
+const month = months[futureDate.getMonth()];
+const weekday = weekdays[futureDate.getDay()];
+
+content.textContent = `Giveaway ends on ${weekday}, ${month} ${date} ${year} at ${hours}:${minutes}pm`;
